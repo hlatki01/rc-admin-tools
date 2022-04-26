ee0cc44db679fe8a94560d88aa9ace6c86042ff7
Luis Hlatki <luis.hlatki@hotmail.com>
2022-04-26 09:10:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const path = require('path')
+
+const axios = require('axios')
+const moment = require('moment')
+var bodyParser = require('body-parser')
+var { Parser } = require('json2csv')
+const { enable } = require('express/lib/application')
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const app = express()
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname,'public')));
+
+
+const port = 5000
+
+
+
+app.get('/', (req, res) => {
+    res.render(__dirname + '/index', {title: '', server: '', username: ''})
+});
+
+app.get('/tools', (req, res) => {
+    res.render(__dirname + '/tools')
+});
+
+app.get('/departments', (req, res) => {
+    res.render(__dirname + '/departments')
+});
+
+app.get('/users', (req, res) => {
+    res.render(__dirname + '/users')
+});
+
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+		console.log('Go to browser and type: http://localhost:5000/export and see the magic happen :)')
+
+})
