71cbc9110469fe38a8da59db8a9a0bc5ef6e43c5
Luis Hlatki <luis.hlatki@hotmail.com>
2022-05-02 12:02:34 -0300

    added bash script

---
+++ init.sh
@@ -0,0 +1,8 @@
+cd ~ &&
+cd rc-admin-tools &&
+docker-compose down &&
+cd .. &&
+sudo rm -r rc-admin-tools &&
+git clone https://github.com/hlatki01/rc-admin-tools rc-admin-tools &&
+cd rc-admin-tools &&
+docker build . --no-cache && docker-compose up -d
