75e168620f7953b933fd5f6626a5eaf179e8c615
Luis Hlatki <luis.hlatki@hotmail.com>
2022-05-03 15:57:53 -0300

    fixed url validation

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const axios = require('axios')
-const moment = require('moment')
-var bodyParser = require('body-parser')
-var { Parser } = require('json2csv')
-const { enable } = require('express/lib/application')
+const axios = require("axios");
+const moment = require("moment");
+var bodyParser = require("body-parser");
+var { Parser } = require("json2csv");
+const { enable } = require("express/lib/application");
+const { log } = require("console");
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const app = express();
+app.set("view engine", "html");
+app.engine("html", require("ejs").renderFile);
+app.use(express.static(path.join(__dirname, "public")));
 
-const app = express()
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
-app.use(express.static(path.join(__dirname,'public')));
+const port = 5000;
 
-
-const port = 5000
-
-
-
-app.get('/', (req, res) => {
-    res.render(__dirname + '/index', {title: '', server: '', username: ''})
+app.get("/", (req, res) => {
+  res.render(__dirname + "/index", { title: "", server: "", username: "" });
 });
 
-app.get('/tools', (req, res) => {
-    res.render(__dirname + '/tools')
+app.get("/tools", (req, res) => {
+  res.render(__dirname + "/tools");
 });
 
-app.get('/departments', (req, res) => {
-    res.render(__dirname + '/departments')
+app.get("/departments", (req, res) => {
+  res.render(__dirname + "/departments");
 });
 
-app.get('/users', (req, res) => {
-    res.render(__dirname + '/users')
+app.get("/users", (req, res) => {
+  res.render(__dirname + "/users");
 });
 
-app.get('/livechat-rooms', (req, res) => {
-    res.render(__dirname + '/livechat-rooms')
+app.get("/livechat-rooms", (req, res) => {
+  res.render(__dirname + "/livechat-rooms");
 });
 
-app.get('/channels', (req, res) => {
-    res.render(__dirname + '/channels')
+app.get("/channels", (req, res) => {
+  res.render(__dirname + "/channels");
 });
-
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
-		console.log('Go to browser and type: http://localhost:5000/export and see the magic happen :)')
-
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(
+    "Go to browser and type: http://localhost:5000/export and see the magic happen :)"
+  );
+});
